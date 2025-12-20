c88fcbea45a2890d45bf84900b1db7dee708abb3
Caia-Tech <owner@caiatech.com>
2025-12-20 07:44:45 -0500

    testing

--- sitecustomize.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Keep this repo's tests isolated from whatever pytest plugins are installed in
-# the user's environment (some require external services / native deps).
-if os.environ.get("PYTEST_DISABLE_PLUGIN_AUTOLOAD") is None:
-    os.environ["PYTEST_DISABLE_PLUGIN_AUTOLOAD"] = "1"
